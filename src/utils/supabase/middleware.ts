4d94a3847ed7bd52d6a4a202475891a18e306f61
Piyapat <piyapat.pai@outlook.com>
2025-10-04 16:43:22 +0700

    feat: adaded routes to exclude from rerouting to login

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -41,9 +41,12 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
+    !request.nextUrl.pathname.startsWith('/') &&
     !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/register') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
     !request.nextUrl.pathname.startsWith('/error')
+    // !request.nextUrl.pathname.startsWith('/') &&
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
