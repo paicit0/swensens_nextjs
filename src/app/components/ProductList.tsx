1710edf3f55856cbdef753db6be63d8c96189dc0
Piyapat <piyapat.pai@outlook.com>
2025-10-07 12:06:24 +0700

    feat: style change

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,49 +1,52 @@
-// src/components/ProductList.tsx (Client Component)
+// src/app/components/ProductList.tsx
 
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-type Product = {
+export type Product = {
   product_id: string;
   product_name: string;
   product_price: number;
+  product_category: string;
 };
 
 export default function ProductList({
-  initialProducts, sortType
+  initialProducts,
+  category,
 }: {
   initialProducts: Product[];
-  sortType: string;
+  category: string;
 }) {
   const [products, setProducts] = useState(initialProducts);
-  const [sortBy, setSortBy] = useState<"name" | "price">("name");
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
 
-  const handleSort = (type: "name" | "price") => {
-    setSortBy(type);
-    const sorted = [...products].sort((a, b) => {
-      if (type === "name") {
-        return a.product_name.localeCompare(b.product_name);
-      }
-      return a.product_price - b.product_price;
-    });
-    setProducts(sorted);
-  };
+  useEffect(() => {
+    if (category) {
+      const filteredProducts = products.filter(
+        (product) => product.product_category === category
+      );
+      setFilteredProducts(filteredProducts);
+    }
+  }, [category]);
 
   return (
     <div className="flex w-full flex-col items-center gap-11">
       <div className="w-full">
         <div className="grid h-fit w-full grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4">
-          {products?.map((item) => (
+          {filteredProducts?.map((item) => (
             <div
               key={item.product_id}
-              className="flex h-auto w-full flex-col justify-between overflow-hidden rounded-lg border border-solid border-gray-200"
+              className="flex h-auto w-full flex-col justify-between overflow-hidden rounded-2xl border border-solid border-gray-200"
             >
               <div className="flex flex-col gap-1">
-                <div className="flex aspect-[4/3] h-auto w-full bg-gray-100 rounded-lg"></div>
-                <div className="mb-3 flex h-fit flex-col gap-1 p-3">
-                  {item.product_price}
-                  {item.product_name}
+                <img className="flex aspect-[4/3] width-[296px] bg-gray-100 rounded-lg"></img>
+                <div className="flex relative h-fit flex-col gap-1 p-3">
+                  <div className="absolute top-[-11px] right-[16px] w-[32px] h-[32px] bg-red-500 rounded-full text-2xl flex justify-center items-center">
+                    <span className="transform -translate-y-[3px] text-white">+</span>
+                  </div>
+                  <div className="text-red-400 font-bold">฿ {item.product_price}</div>
+                  <div className="font-bold">{item.product_name}</div>
                 </div>
               </div>
             </div>
